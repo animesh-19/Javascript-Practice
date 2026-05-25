56de4d919df61d5e7b60db9e6fe26a6f215688e6
animesh-19 <animeshpyakurel@gmail.com>
2026-05-25 22:18:14 +0545

    Add project files

---
+++ Day1.js
@@ -0,0 +1,57 @@
+// // // To print the code 
+// // console.log("Animesh Pyakurel")
+
+
+// // // Variable creation 
+// // name="Animesh Pyakurel"
+// // console.log(name)
+// // let age =22
+// // console.log(age)
+// // x= null
+// // console.log(x)
+// // y=undefined
+// // console.log(y)
+// // isStatus=true;
+// // console.log(isStatus)
+
+
+// // // keywords in js 
+// // age =23;
+// // age =77;
+// // age=74;
+// // console.log(age);
+
+// // // const pi = 3.14; # to fix the value 
+
+// // // // Data tyupes in java script 
+// // let age = 56;
+// // console.log(age);
+
+// // let name = "Animesh";
+// // console.log(name);
+
+// // let ismarried=true;
+// // console.log(ismarried);
+// // let x=null;
+// // console.log(x);
+
+// // let y=undefined;
+// // console.log(y);
+
+// // Objects 
+// const Employee={
+//     name:"Animesh Pyakurel",
+//     age:22,
+//     company:"Novelty Labs",
+//     Deginsation:"Frontnd Developer"
+// };
+
+// To add the pen info in js 
+
+const pen={
+    name:'Pilot pen',
+    penRating:"3 star",
+    Price:100,
+    offer:"20% off"
+}
+console.log(pen);
