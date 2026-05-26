83c354012eee11770046893194d62c6a68dacbb0
animesh-19 <animeshpyakurel@gmail.com>
2026-05-26 21:41:36 +0545

    Add Day 2 practice and update entry point

---
+++ Day2.js
@@ -0,0 +1,97 @@
+// //operators in js 
+// //1. Arithmetic operators
+
+// let a=55;
+// let b=60;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+// console.log("_a++=",a++);
+// console.log("a+=4",a+=4);
+
+// //2.Compariion operators
+// let a=10;
+// let b=40;
+// console.log("a!=b:",a!=b);
+// console.log("a=b:",a=b)
+// console.log("a==b",a==b);
+// console.log("a!==b",a!==b);
+// console.log("a>b",a>b)
+
+// // 3.Logical operators 
+// let x=10
+// let y= 7;
+// let condtn1=x>y;
+// let condtn2=x=y;
+// console.log("condtn1 && condtn2",condtn1 && condtn2);
+// console.log("condtn1 || condtn2",condtn1 || condtn2);
+// console.log("!condtn1",!condtn1);
+
+
+//Conditional Statements
+// 1.if statements
+// let age=10;
+
+// if(age>=18){
+//     console.log("You are eligilbe for he vote ");
+// }
+
+// if(age<18){
+//     console.log("You are not eligilbe for the vote ");
+// }
+
+// let mode='dark';
+// let color;
+
+// if(mode=='dark'){
+//     color='black' 
+// }
+// if(mode=='bright'){
+//     color='white' 
+// }
+// console.log('',color);
+
+
+// let mode='bright';
+// let color;
+// if(mode=='light'){
+//     color="white"
+// }else{
+//     color='black';
+// }
+// console.log(color);
+
+// let a=10;
+// if(a%2===0){
+//     console.log(a,"is even number");
+// }else{
+//     console.log(a,"is odd number");
+// }
+
+
+// let a=10;
+// let b=20;
+// let c=2;
+
+// if(a>b && a>c){
+//     console.log(a,'is greatest number');
+// }else if(b>a && b>c){
+//     console.log(b,'is greatest number');
+// }else{
+//     console.log(c,'is greatest number');
+// }
+
+let mode='';
+let color;
+
+if(mode=='light'){
+    color='white';
+}else if (mode=='dark'){
+    color='black';
+}else{
+    color="blue";
+}
+console.log(color,'mode is selected');
