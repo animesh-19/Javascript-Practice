69f29bb9eae2a09affede2d5beec5f14136077a1
animesh-19 <animeshpyakurel@gmail.com>
2026-06-02 22:30:41 +0545

    Publish UI files: index, styles, temperature converter, and functions

---
+++ functions.js
@@ -0,0 +1,74 @@
+// //function is a block of code that performs the specific task when the function is called.
+
+// function animesh(){
+//     console.log("my name is animesh  from the capital city of nepal k");
+//     console.log("This is the financial capital of the nepal ");
+
+// }
+
+// animesh();
+
+// // declaring  the functions by passing the parameters 
+// // function animesh(username,age,city ){
+// //     console.log(`My name is ${username} and I am ${age} years old and I live in ${city}`);
+// // }
+// // animesh('animesh',34,'kathmandu');
+
+
+// // function add(a,b,c){
+// //     let sum=a+b+c;
+// //     console.log('The sum of the three numbers is :' + sum);
+// // }
+// // add(10,20,45);
+
+// function animeshDetails(name,age,city,feild){
+//     console.log(`My name is ${name} and I am ${age} years old and I have been living in ${city} and I have great interest in ${feild}.`)
+//     console.log('My name is '+name+' and I am '+age+' years old and I have been living in '+city+' and I have great interest in '+feild);
+// }
+
+
+// // animeshDetails('animesh',34,'Biratnagar','Web development');
+
+// // function add(x,y){
+
+// let sum =x+y;
+// return sum;
+// }
+// console.log(add(20,40));
+
+// to check the number is even or not 
+
+// function isEven(num){
+//     return num%2===0 ?true:false;
+// }
+// console.log(isEven(10));
+
+
+
+// function isEven (number){
+//     if(number%2===0){
+//         return true;
+//     } else {
+// //         return false;
+// //     }
+// // }
+// // console.log(isEven(11));
+
+
+// //checking the validity of the email 
+//  function isValidEmail(email){
+//     if (email.includes('@') && email.includes('.')){
+//         return true;
+//     }else{
+//         return false;
+//     }
+
+// }
+// console.log(isValidEmail('animeshgmaail.com'));
+
+// //using ternary operator 
+// function isValidEmail(email){
+//     return email.includes('@')&&email.includes('.')?true:false;
+// }
+// console.log(isValidEmail('animesh@pyakurelgmail.com'))
+
